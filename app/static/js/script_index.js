09898148c9be1ffa73d67847fa4d3c50e4968636
Arthur <arthurreisgonc@gmail.com>
2024-11-03 01:24:16 -0300

    Feat: estilização

--- app/static/js/script_index.js
+++ app/static/js/script_index.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('#qtde_alert').forEach(td => {
     if (td.innerText == "0") { // Verifica se o conteúdo do td é "0"
-        td.style.background = 'red'; // Altera o background para vermelho
+        td.style.background = '#FF6666'; // Altera o background para vermelho
     }
 });
