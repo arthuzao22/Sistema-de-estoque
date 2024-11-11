2e7cebc2b632a73ddd7109d13d2adc17a311ec5f
Arthur Reis <110342375+arthuzao22@users.noreply.github.com>
2024-11-11 11:24:20 -0300

    bugs: bug de cadastro arrumado

--- app/static/js/script_form.js
+++ app/static/js/script_form.js
@@ -1,8 +1,8 @@
   // Função para exibir o spinner ao enviar o formulário
   function showLoadingSpinner() {
     document.getElementById('loading-spinner').style.display = 'block';
-    document.querySelector('.card').style.display = 'none';  // Esconde o card de login
-    document.querySelector('button[type="submit"]').disabled = true;  // Desabilita o botão de submit
+    document.querySelector('.card').style.display = 'none'; 
+    document.querySelector('button[type="submit"]').disabled = true; 
 }
 
 
